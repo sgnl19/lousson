ac4a2d7d2b906353564b54a00fd420086bd07e6c
Matze <mhennig@quirkies.org>
2012-04-21 18:57:54 +0200

    imported the Lousson\WSDL interfaces

---
+++ src/Lousson/WSDL/AnyBindingOperation.php
@@ -0,0 +1,45 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 textwidth=75: *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright (c) 2011 Mathias J. Hennig                                  *
+ *                                                                       *
+ * Permission to use, copy, modify, and/or distribute this software for  *
+ * any purpose with or without fee is hereby granted, provided that the  *
+ * above copyright notice and this permission notice appear in all       *
+ * copies.                                                               *
+ *                                                                       *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL         *
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED         *
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE      *
+ * AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL  *
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR *
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER        *
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR      *
+ * PERFORMANCE OF THIS SOFTWARE.                                         *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+namespace Lousson\WSDL;
+
+/**
+ *  Definition of the Lousson\WSDL\AnyBindingOperation interface.
+ */
+use Lousson\WSDL\AnyComponent;
+
+/**
+ *  The Lousson\WSDL\AnyBindingOperation interface declares the API
+ *  required in order to represent WSDL binding operation components.
+ *
+ *  @package    org.lousson.wsdl
+ *  @since      December 2011
+ *
+ *  @see http://www.w3.org/TR/wsdl20/#Binding_Operation
+ *  @see http://www.w3.org/TR/wsdl20/#component_model
+ */
+interface AnyBindingOperation extends AnyComponent
+{
+    function getInterfaceOperation();
+    function getBindingMessageReferences();
+    function getBindingFaultReferences();
+    function getParent();
+}
+
---
+++ src/Lousson/WSDL/AnyComponent.php
@@ -0,0 +1,46 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 textwidth=75: *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright (c) 2011 Mathias J. Hennig                                  *
+ *                                                                       *
+ * Permission to use, copy, modify, and/or distribute this software for  *
+ * any purpose with or without fee is hereby granted, provided that the  *
+ * above copyright notice and this permission notice appear in all       *
+ * copies.                                                               *
+ *                                                                       *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL         *
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED         *
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE      *
+ * AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL  *
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR *
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER        *
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR      *
+ * PERFORMANCE OF THIS SOFTWARE.                                         *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+namespace Lousson\WSDL;
+
+/**
+ *  Definition of the Lousson\WSDL\AnyComponent interface.
+ *
+ *  @package    org.lousson.wsdl
+ *  @since      December 2011
+ *  @copyright  2011 Mathias J. Hennig
+ *  @license    http://www.opensource.org/licenses/ISC ISC License
+ *  @author     Mathias J. Hennig <mhennig at quirkies.org>
+ */
+use Lousson\WSDL;
+
+/**
+ *  The Lousson\WSDL\AnyComponent interface is the base for all WSDL
+ *  component API declarations.
+ *
+ *  @package    org.lousson.wsdl
+ *  @since      December 2011
+ *
+ *  @see http://www.w3.org/TR/wsdl20/#component_model
+ */
+interface AnyComponent
+{
+}
+
